44058e9e7c8e8e869599d2eee75bb6c79d17c5b9
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-01 14:47:07 +0530

    sdc

---
+++ mysql.tf
@@ -0,0 +1,68 @@
+# Creating RDS : MySQL
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin1"
+  password             = "RoboShop1"
+  parameter_group_name = aws_db_parameter_group.mysql_pg.name
+  skip_final_snapshot  = true
+  db_subnet_group_name    = aws_db_subnet_group.mysql_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.allows_mysql.id]
+}
+
+# creates subnet group 
+resource "aws_db_subnet_group" "mysql_subnet_group" {
+  name       = "roboshop-mysql-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-mysql-${var.ENV}-subnetgroup"
+  }
+}
+
+resource "aws_db_parameter_group" "mysql_pg" {
+  name   = "roboshop-${var.ENV}-mysql-pg"
+  family = "mysql5.7"
+}
+
+
+
+
+
+
+
+
+# Creating Elastic Cache Cluster : Redis
+/* resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "roboshop-${var.ENV}-redis"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
+  engine_version       = "6.x"
+  port                 = 6379 
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids   = [aws_security_group.allows_redis.id]   
+}
+
+# Creating Parameter Group needed for Elastic Cache
+resource "aws_elasticache_parameter_group" "redis_pg" {
+  name   = "roboshop-${var.ENV}-redis-pg"
+  family = "redis6.x"
+}
+
+
+# creates subnet group 
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "roboshop-redis-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-redis-${var.ENV}-subnetgroup"
+  }
+} */
+
+
