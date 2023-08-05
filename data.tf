b3091f8dc129b4b32fa371aa1c9d07ac05a3f781
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-05 13:12:36 +0530

    sdc

--- data.tf
+++ data.tf
@@ -14,7 +14,6 @@ data "aws_secretsmanager_secret" "secrets" {
   name = "robot/secrets"
 }
 
-
 data "aws_secretsmanager_secret_version" "secret_version" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
